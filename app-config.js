13469fe132a3b6d128dece5474edfc359c2fe231
ahtechnology <annahorvathtech@gmail.com>
2026-08-02 17:32:25 +0200

    v1.0

--- app-config.js
+++ app-config.js
@@ -1,13 +1,23 @@
 window.APP_CONFIG = Object.freeze({
     conditionSeparation: 11,
-    totalSessions: 15,
-    imagesPerSession: 9,
+    //totalSessions: 15,  // nem számít
+    //imagesPerSession: 9,  // nem számít
     imageDurationMs: 30000,
     //supabaseUrl: 'https://ofncvazlwzanexavgnur.supabase.co',
     //supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9mbmN2YXpsd3phbmV4YXZnbnVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0NjUyNzIsImV4cCI6MjEwMDA0MTI3Mn0.xKBZOgjZc6lcpk5Ubp9nX69z3POUb-o9RGlkEzTh2MU',
     supabaseUrl: 'https://asfiryvbysugacifjkhm.supabase.co',
     supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzZmlyeXZieXN1Z2FjaWZqa2htIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MTM3MjksImV4cCI6MjEwMTA4OTcyOX0.XSkOksCKZjudD79yhTZQV4DO4B6bwUda1nEnLYGQsHI',
-    adminEmailPrefix: 'admin@',
+    adminEmailPrefix: 'admin@',  // Supabase admin user
+    localUsers: Object.freeze({  // local userek
+        testadmin: Object.freeze({
+            password: '456',
+            role: 'admin'
+        }),
+        testuser: Object.freeze({
+            password: '123',
+            role: 'user'
+        })
+    }),
     heatmapOverlayAlpha: 125,
     absoluteHeatmapMaxRevealTimeMs: 6000,  // kép
     heatmapHistogramBinWidthMs: 250,  // dX
